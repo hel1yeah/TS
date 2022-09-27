dc6b09ce62d5037118a7efaef83fc566ddae9db6
hel1_yeah <vihura.yurii@tftc.dev>
2022-09-27 15:17:51 +0300

    add ts + first lesson

---
+++ ts/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let revenue = 1000;
+let bonus = 500;
+let res = revenue + bonus;
+console.log(res);
---
+++ ts/app.ts
@@ -0,0 +1,6 @@
+let revenue:number = 1_000
+let bonus:number = 500
+
+let res: number = revenue + bonus
+
+console.log(res);
