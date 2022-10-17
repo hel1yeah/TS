b88bb05d162fe43213fcf043fffb817dc0b64606
hel1_yeah <vihura.yurii@tftc.dev>
2022-10-17 20:04:33 +0300

    readonly

--- ts/app.ts
+++ ts/app.ts
@@ -1,7 +1,6 @@
 //  typles = кортедж
 
-const skill = [1, 'Front']
+const skill: readonly ( number | string)[] = [1, 'Front']
 
-const [id, SkillName] = skill
 
-const arr: [number, string, ...boolean[]] = [1, 'Front', true, false]
+const fruit: ReadonlyArray<string> = ['red', 'green', 'yellow']
