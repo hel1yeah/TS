58b30565d791be6a5b7886ae5a268f290fce492c
hel1_yeah <vihura.yurii@tftc.dev>
2022-10-17 15:54:59 +0300

    types in function

--- ts/app.js
+++ ts/app.js
@@ -1,5 +1,14 @@
 "use strict";
-const red = 'red';
-const num = 1;
-let res = red + ' ' + num;
-console.log(res);
+function getFullName(fN, sN) {
+    if (typeof fN !== 'string') {
+        throw new Error('!!!!!');
+    }
+    return `${fN} ${sN}`;
+}
+const arrFunctions = (one, twoo) => {
+    return `${one} ${twoo} Arrow`;
+};
+arrFunctions('Yuri', 'Larsen');
+getFullName('Yuri', 'Larsen');
+console.log(arrFunctions('Yuri', 'Larsen'));
+console.log(getFullName('true', 'false'));
--- ts/app.ts
+++ ts/app.ts
@@ -1,7 +1,16 @@
-const red = 'red'
-const num:number = 1
-const num2 = 2
+function getFullName(fN:string, sN:string):string {
+  if(typeof fN !== 'string') {
+    throw new Error('!!!!!')
+  }
+  return `${fN} ${sN}`
+}
 
+const arrFunctions = (one: string, twoo: string):string => {
+  return `${one} ${twoo} Arrow`
+}
 
-let res:number = num2 + num
-console.log(res);
+arrFunctions('Yuri', 'Larsen')
+getFullName('Yuri', 'Larsen')
+
+console.log(arrFunctions('Yuri', 'Larsen'));
+console.log(getFullName('true', 'false'));
