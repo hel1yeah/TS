2a38a1aac84afef8deadb2a80aa945c8bf3d65c0
hel1_yeah <vihura.yurii@tftc.dev>
2022-10-17 16:10:06 +0300

    test

--- ts/app.js
+++ ts/app.js
@@ -1,11 +1,2 @@
 "use strict";
-function getFullName(user) {
-    return `${user.first} ${user.city}`;
-}
-const user = {
-    first: 'Yuri',
-    city: 'Brovary',
-    age: 33,
-    red: 'red',
-};
-console.log(getFullName(user));
+let test;
--- ts/app.ts
+++ ts/app.ts
@@ -1,13 +1,20 @@
-function getFullName(user: { first: string; city: string }): string {
-  return `${user.first} ${user.city}`;
+let test: {
+  officeId: number,
+  isOpened: false,
+  contacts: {
+    phone: string,
+    email: string,
+    address: {
+      city: string,
+    }
+} = {
+	"officeId": 45,
+	"isOpened": false,
+	"contacts": {
+		"phone": "+79100000000",
+		"email": "my@email.ru",
+		"address": {
+			"city": "Москва"
+		}
+	}
 }
-
-const user = {
-  first: 'Yuri',
-  city: 'Brovary',
-  age: 33,
-  red: 'red',
-};
-
-
-console.log(getFullName(user));
